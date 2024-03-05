56635ca6eb0070358ce3ad1da60de3ec14f5281f
emmadev <macanthonyemmanuel12@gmail.com>
2024-03-05 22:20:16 +0100

    implemented a basic hello world function with tdd

---
+++ hello_world/hello.go
@@ -0,0 +1,33 @@
+package main
+
+//import "fmt"
+
+const englishHelloPrefix = "Hello, "
+const spanishPrefix = "Hola, "
+const spanish = "Spanish"
+const frenchPrefix = "Bonjour, "
+const french = "French"
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishPrefix
+	case french:
+		prefix = frenchPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func main() {
+	//fmt.Println(Hello("world"))
+}
