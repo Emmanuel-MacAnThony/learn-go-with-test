1d02e20e0c8be3cb48a3e2a4852ec4e1933862d4
emmadev <macanthonyemmanuel12@gmail.com>
2024-03-07 20:59:29 +0100

    explored mocking

---
+++ src/mocking/countdown.go
@@ -0,0 +1,40 @@
+package countdown
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+const countdownStart = 3
+const finalWord = "Go!"
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct {
+}
+
+type ConfigurableSleeper struct {
+	sleep    func(time.Duration)
+	duration time.Duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+func Countdown(writer io.Writer, sleeper Sleeper) {
+
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(writer, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(writer, finalWord)
+
+}
